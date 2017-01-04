421144b40f2ee75ef4be2eceb744a2d053942392
Ahmed Kamel Taha <ahmedengu@gmail.com>
2017-01-04 11:17:00 +0200

    fix the timer

--- src/com/sleebus/app/controller/UtilsFacade.java
+++ src/com/sleebus/app/controller/UtilsFacade.java
@@ -11,6 +11,8 @@ import java.util.TimerTask;
  * Created by ahmedengu.
  */
 public class UtilsFacade {
+    static Timer timer;
+
     private UtilsFacade() {
     }
 
@@ -29,36 +31,41 @@ public class UtilsFacade {
     }
 
     public static void makingNoise(String id) {
+        makingNoise(id, 100);
+    }
+
+    public static void makingNoise(String id, int delay) {
         Display.getInstance().scheduleBackgroundTask(new Runnable() {
             @Override
             public void run() {
                 Alarm alarm = AlarmDaoImpl.getInstance().getFromAlarms(id);
-                makingNoise(alarm);
+                makingNoise(alarm, delay);
             }
         });
     }
 
-    public static void makingNoise(Alarm alarm) {
-        makingNoise(alarm, 100);
-    }
-
-
     public static void makingNoise(Alarm alarm, int delay) {
-        if (alarm.getState().getStateName().equals("Activited")) {
+        getTimerInstance().cancel();
+        if (!alarm.getState().getStateName().equals(AlarmStateFactory.STATES[AlarmStateFactory.DISABLED])) {
             if (alarm.isVibrate())
                 Display.getInstance().vibrate(3);
             if (alarm.isFlashlight())
                 Display.getInstance().flashBacklight(3);
             if (alarm.isSound())
                 Display.getInstance().playBuiltinSound(Display.SOUND_TYPE_ALARM);
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
+
+            getTimerInstance().schedule(new TimerTask() {
                 @Override
                 public void run() {
-                    makingNoise(alarm.getId());
+                    makingNoise(alarm.getId(), delay);
                 }
             }, delay);
         }
     }
 
+    public static synchronized Timer getTimerInstance() {
+        if (timer == null)
+            timer = new Timer();
+        return timer;
+    }
 }
